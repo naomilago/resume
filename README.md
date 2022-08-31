ed78c8a6fbc053a3f73e4d113bf7eef5dc4cb317
Naomi Lago <naomilago6@gmail.com>
2022-08-31 00:14:45 -0300

    github is nice ✨

---
+++ README.md
@@ -0,0 +1,44 @@
+# This is my Digital Resumé
+I created a digital resume in minutes with Python and Streamlit. For this, I followed a tutorial by <a href='https://github.com/Sven-Bo/digital-resume-template-streamlit'>@Sven-Bo</a>.
+
+Feel free to use this template and adjust it to your needs & linking.<br>
+
+💡 **The website design is inspired by [@divanov11](https://github.com/divanov11)** 💡 <br>
+Find his:
+- repository here: https://github.com/divanov11/Digital-Resume
+- YouTube video here: https://youtu.be/clwpf3VwCZQ
+
+## Production view:
+👉 **https://naomilago.com**
+
+## Screenshots
+![Demo1](./assets/demo1.png?raw=true "Demo1")
+![Demo2](./assets/demo2.png?raw=true "Demo2")
+![Demo3](./assets/demo3.png?raw=true "Demo3")
+
+## Video Tutorial
+[![YouTube Video](https://img.youtube.com/vi/BXAeMICmUSQ/0.jpg)](https://youtu.be/BXAeMICmUSQ)
+
+
+## Requirements
+Install the dependencies with pip
+```
+Pillow==9.2.0
+streamlit==1.12.0
+```
+
+## Run the app
+Terminal
+```
+# vanilla terminal
+streamlit run app.py
+```
+
+## About Me
+I'm a Data Science Analyst at [Nestlé](https://www.nestle.com/). I'm passionate about data science and machine learning. 
+- Linkedin: https://www.linkedin.com/in/naomilago/
+- Twitter: https://twitter.com/naomilago
+- Website: https://pythonandvba.com
+
+## Feedback
+If you have any feedback, please reach out to me at info@naomilago.com
---
+++ app.py
@@ -0,0 +1,157 @@
+from streamlit_lottie import st_lottie
+from pathlib import Path
+import streamlit as st
+from PIL import Image
+import requests
+
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "Naomi Lago - CV.pdf"
+profile_pic = current_dir / "assets" / "avatar.png"
+
+
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Naomi Lago"
+PAGE_ICON = './assets/favicon.png'
+NAME = "Naomi Lago"
+DESCRIPTION = """
+Data Science Analyst, assisting and supporting data-driven decision-making.
+"""
+EMAIL = "info@naomilago.com"
+SOCIAL_MEDIA = {
+    "LinkedIn": "https://linkedin.com/in/naomilago",
+    "GitHub": "https://github.com/naomilago",
+    "Twitter": "https://twitter.com/naomilago",
+}
+PROJECTS = {
+    "🏆 Pet Paws - A pet donation friendly system": "https://github.com/naomilago/Pet-Paws",
+    "🏆 Personal Blog - A personal blog built using Hugo": "https://github.com/naomilago/blog   ",
+    "🏆 Lucky Answers - An Android app made with Flutter that aims to answer simple questions": "https://github.com/naomilago/Lucky-Answer",
+    "🏆 ExPlan - An Android app made with Flutter that can help you plan your expenses": "https://github.com/naomilago/ExPlan",
+}
+
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+def load_lottie(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+datascience_animation = load_lottie('https://assets10.lottiefiles.com/packages/lf20_qp1q7mct.json')
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",
+    )
+    st.write("📫", EMAIL)
+
+with col2:
+    # st.image(profile_pic, width=230)
+    st_lottie(datascience_animation)
+
+
+# --- SOCIAL LINKS ---
+st.write("#")   
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write("#")
+st.subheader("Experience & Qulifications")
+st.write(
+    """
+- ✔️ 5 years studying technology
+- ✔️ Hands on experience and knowledge in Python and Excel
+- ✔️ Good understanding of statistical principles and their respective applications
+- ✔️ Great team-player and displaying strong sense of initiative on tasks
+"""
+)
+
+
+# --- SKILLS ---
+st.write("#")
+st.subheader("Hard Skills")
+st.write(
+    """
+- 👩‍💻 Programming: Python (Scikit-learn, Pandas, Numpy), SQL, JavaScript (React, React-Native)
+- 📊 Data Visulization: PowerBi, MS Excel, Plotly, Streamlit
+- 📚 Modeling: Linear Regression
+- 🗄️ Databases: Postgres, MongoDB, MySQL
+- ☁ Cloud: GCP, AWS, Azure, Linode, Databricks
+"""
+)
+
+
+# --- WORK HISTORY ---
+st.write("#")
+st.subheader("Work History")
+st.write("---")
+
+# --- JOB 1
+st.write("🚧", "**Data Science Analyst | Nestlé**")
+st.write("07/2022 - Present")
+st.write(
+    """
+- ► Use of Python to manipulate and prepare data for analysis
+- ► Use of Pandas and Plotly to create data visualizations
+- ► Use of Streamlit to create interactive web apps
+- ► Use of SQL to create and manage databases
+- ► Use of Databricks to host and manage Jupyter notebooks
+- ► Use of Azure to host and manage cloud services
+"""
+)
+
+# --- JOB 2
+st.write("#")
+st.write("🚧", "**UX/UI Designer | Cliqx**")
+st.write("01/2018 - 02/2022")
+st.write(
+    """
+- ► Designing and implementing user interfaces for web and mobile apps
+- ► Creating wireframes and prototypes for web and mobile apps
+- ► Creating mockups for web and mobile apps
+- ► Creating user stories and user flows for web and mobile apps
+"""
+)
+
+# --- JOB 3
+st.write("#")
+st.write("🚧", "**Help Desk Analyst | Cliqx**")
+st.write("04/2015 - 01/2018")
+st.write(
+    """
+- ► Solving customer issues and providing assistance to customers
+- ► Assisting customers with software-technical issues
+- ► Dealt with APIs, Databases, bot services flows
+"""
+)
+
+
+# --- Projects & Accomplishments ---
+st.write("#")
+st.subheader("Projects & Accomplishments")
+st.write("---")
+for project, link in PROJECTS.items():
+    st.write(f"[{project}]({link})")
